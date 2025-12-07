4058e45996f8d880e44c8d575366ae9d188ecb80
Kaif Saiyed <kaifsaiyed896@gmail.com>
2025-12-07 19:31:41 +0530

    fixing deployment bugs - 6

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Header from "@/app/components/header/Header";
 import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
@@ -10,66 +13,4 @@ export default function Home() {
       <Footer />
     </main>
   );
-}
-
-{
-  /* <div className={styles.page}>
-  <main className={styles.main}>
-    <Image
-      className={styles.logo}
-      src="/next.svg"
-      alt="Next.js logo"
-      width={100}
-      height={20}
-      priority
-    />
-    <div className={styles.intro}>
-      <h1>To get started, edit the page.tsx file.</h1>
-      <p>
-        Looking for a starting point or more instructions? Head over to{" "}
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Templates
-        </a>{" "}
-        or the{" "}
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning
-        </a>{" "}
-        center.
-      </p>
-    </div>
-    <div className={styles.ctas}>
-      <a
-        className={styles.primary}
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          className={styles.logo}
-          src="/vercel.svg"
-          alt="Vercel logomark"
-          width={16}
-          height={16}
-        />
-        Deploy Now
-      </a>
-      <a
-        className={styles.secondary}
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Documentation
-      </a>
-    </div>
-  </main>
-</div>; */
 }
